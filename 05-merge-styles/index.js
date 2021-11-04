0119e64a22f0b8ae0380b352c1c97f2bc5325e60
elenaBezro <lenyka_86@mail.ru>
2021-11-05 00:18:40 +0100

    feat: add 5th task solution

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,35 @@
+const fs = require("fs");
+const path = require("path");
+const { Stream } = require("stream");
+
+const stylesFolderPath = path.join(__dirname, "styles");
+const bundleStylesPath = path.join(__dirname, "project-dist", "bundle.css");
+const bundleStyles = fs.createWriteStream(bundleStylesPath);
+
+fs.readdir(stylesFolderPath, { withFileTypes: true }, (err, files) => {
+  if (err) throw err;
+
+  let i = 0;
+  const writeData = () => {
+    const file = files[i];
+    if (path.extname(file.name) === ".css") {
+      const stream = new fs.ReadStream(path.join(stylesFolderPath, file.name));
+      stream.on("readable", (err) => {
+        if (err) throw err;
+
+        const data = stream.read();
+        if (data) {
+          bundleStyles.write(data);
+        }
+        i++;
+        if (i < files.length) {
+          writeData();
+        }
+      });
+      stream.on("error", (err) => {
+        throw err;
+      });
+    }
+  };
+  writeData(); 
+});
